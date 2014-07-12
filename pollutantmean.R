dc2b65f2ef14900ea340f1dfc795dc915a0f9c96
ian <ianfmc@gmail.com>
2014-07-11 17:57:43 -0700

    tuned file reads, and formatted output to match results of unit test case.

--- pollutantmean.R
+++ pollutantmean.R
@@ -12,39 +12,40 @@ pollutantmean <- function(directory, pollutant, id=1:332) {
                             nitrate=numeric(0), 
                             ID=integer(0))
     
-    ## loop through and append all values for the selected files
-    ## and the selected pollutant
-    
     if (pollutant == "sulfate") {
+      
+      ## loop through and append all values for the selected files
+      ## and the selected pollutant
+      
       for (i in id) {
         filename <- paste(directory, sprintf("%03d", i), sep="")
         filename <- paste(filename, ".csv", sep="")
         
-        current_values <- read.csv(filename, colClasses=c("character",
-                                                          "numeric",
-                                                          "numeric",
-                                                          "integer")) 
+        current_values <- read.csv(filename) 
         current_values_clean <- subset(current_values,!is.na(current_values$sulfate))
+        
         all_values <- rbind(all_values, current_values_clean)
       } 
       pollutant_mean <- mean(all_values[,"sulfate"], na.rm=TRUE)
     }
     else {
       for (i in id) {
+        
+        ## loop through and append all values for the selected files
+        ## and the selected pollutant
+        
         filename <- paste(directory, sprintf("%03d", i), sep="")
         filename <- paste(filename, ".csv", sep="")
         
-        current_values <- read.csv(filename, colClasses=c("character",
-                                                          "numeric",
-                                                          "numeric",
-                                                          "integer")) 
+        current_values <- read.csv(filename) 
         current_values_clean <- subset(current_values,!is.na(current_values$nitrate))
+        
         all_values <- rbind(all_values, current_values_clean)        
       }
       pollutant_mean <- mean(all_values[,"nitrate"], na.rm=TRUE)    
     }
-      
+    
     ## print the mean of the selected pollutant 
-    print(pollutant_mean)
+    sprintf("%4.3f", pollutant_mean)
   }
 }
